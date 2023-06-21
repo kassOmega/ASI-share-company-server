afff01d39409ac5ee74995579206e06eec9fafac
Zemichael Tesfaye <zzimick@gmail.com>
2023-06-21 10:34:40 +0300

    Update db.js
    DB uses env variables

--- src/database/db.js
+++ src/database/db.js
@@ -2,11 +2,11 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize({
   dialect: "mysql",
-  host: "localhost",
-  port: 3306,
-  username: "root",
-  password: "root",
-  database: "asi-share",
+  host: process.env.DB_HOST | "localhost",
+  port: process.env.DB_PORT | 3306,
+  username: process.env.DB_USER | "root",
+  password: process.env.DB_PASS | "root",
+  database: process.env.DB_NAME | "asi-share",
   logging: false,
 });
 
