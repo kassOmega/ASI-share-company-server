5f53e177ec038a10e369d76004765e206b40eba7
kassahun <kass3me@gmail.com>
2023-06-22 12:41:48 +0300

    fix: register customer fixed

--- src/models/type.js
+++ src/models/type.js
@@ -3,8 +3,8 @@ const registerCustomerSchema = z.object({
   fullName: z.string().min(1, "full name is required"),
   phoneNumber: z.string().min(1, "phone number is required"),
   address: z.string().min(1, "phone number is required"),
-  password: z.string().min(1, "password is required"),
-  userName: z.string().min(1, "user name is required"),
+  // password: z.string().min(1, "password is required"),
+  // userName: z.string().min(1, "user name is required"),
   totalSharePromised: z.number(),
   totalSharePaid: z.number(),
 });
