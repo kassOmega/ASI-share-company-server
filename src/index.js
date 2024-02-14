2b92c8188cb030071b63b358f8b0026b8e647ac2
Kassahun Takele <kass3me@gmail.com>
2024-02-14 13:47:28 +0300

    CORS added

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ require("./database");
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.use("/api/customer", require("./routes/customer"));
 app.use("/api/admin", require("./routes/admin"));
